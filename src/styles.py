40944731d063f9e4d97201dea5ceb9d6e3ad2b0a
Abel Tomás <abeltomasr98@gmail.com>
2024-10-17 15:42:28 -0300

    0.3.3 - setuptools added; requirements and .gitignore updated; repository color changes

--- src/styles.py
+++ src/styles.py
@@ -6,44 +6,44 @@ console = Console()
 
 
 def push_event_message(size, repo_name):
-    message = Text.from_markup(f"- [bold #8ac209]Pushed {size} commits[/bold #8ac209] to [italic underline blue]{repo_name}[/italic underline blue]")
+    message = Text.from_markup(f"- [bold #8ac209]Pushed {size} commits[/bold #8ac209] to [italic dim]{repo_name}[/italic dim]")
     console.print(message, end='')
 
 
 def watch_event_message(repo_name):
-    message = Text.from_markup(f"- [bold #ffca41]Starred[/bold #ffca41] [italic underline blue]{repo_name}[/italic underline blue]")
+    message = Text.from_markup(f"- [bold #ffca41]Starred[/bold #ffca41] [italic dim]{repo_name}[/italic dim]")
     console.print(message, end='')
 
 
 def create_event_message(ref_type, ref, repo_name):
-    message = Text.from_markup(f"- [bold #00ffff]Created {ref_type} '{ref}'[/bold #00ffff] in [italic underline blue]{repo_name}[/italic underline blue]")
+    message = Text.from_markup(f"- [bold #00ffff]Created {ref_type} '{ref}'[/bold #00ffff] in [italic dim]{repo_name}[/italic dim]")
     console.print(message, end='')
 
 
 def delete_event_message(ref_type, ref, repo_name):
-    message = Text.from_markup(f"- [bold red]Deleted {ref_type} '{ref}'[/bold red] in [italic underline blue]{repo_name}[/italic underline blue]")
+    message = Text.from_markup(f"- [bold red]Deleted {ref_type} '{ref}'[/bold red] in [italic dim]{repo_name}[/italic dim]")
     console.print(message, end='')
 
 
 def fork_event_message(repo_name, forkee_full_name):
-    message = Text.from_markup(f"- [bold]Forked [italic underline blue]{repo_name}[/italic underline blue] to [italic underline blue]{forkee_full_name}[/italic underline blue][/bold]")
+    message = Text.from_markup(f"- [bold]Forked [italic underline dim]{repo_name}[/italic dim] to [italic blue]{forkee_full_name}[/italic underline blue][/bold]")
     console.print(message, end='')
 
 
 def issue_event_message(repo_name):
-    message = Text.from_markup(f"- [bold]Opened a new issue in [italic underline blue]{repo_name}[/italic underline blue][/bold]")
+    message = Text.from_markup(f"- [bold]Opened a new issue in [italic dim]{repo_name}[/italic dim][/bold]")
     console.print(message, end='')
 
 
 def release_event_message(repo_name, release_name):
-    message = Text.from_markup(f"- [bold]Published a new release in [italic underline blue]{repo_name}[/italic underline blue]: [bold]{release_name}[/bold]")
+    message = Text.from_markup(f"- [bold]Published a new release in [italic dim]{repo_name}[/italic dim]: [bold]{release_name}[/bold]")
     console.print(message, end='')
 
 
 def pull_request_event_message(action, repo_name):
     if action == "closed":
-        message = Text.from_markup(f"- [bold violet]Merged a pull request[/bold violet] in [italic underline blue]{repo_name}[/italic underline blue]")
+        message = Text.from_markup(f"- [bold violet]Merged a pull request[/bold violet] in [italic dim]{repo_name}[/italic dim]")
         console.print(message, end='')
     else:
-        message = Text.from_markup(f"- [bold]Opened a pull request[/bold] in [italic underline blue]{repo_name}[/italic underline blue]")
+        message = Text.from_markup(f"- [bold]Opened a pull request[/bold] in [italic dim]{repo_name}[/italic dim]")
         console.print(message, end='')
