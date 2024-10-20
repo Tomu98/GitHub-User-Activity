43000c253f767aef13a3a0a1df0b29d4944d52e2
Abel Tomás <abeltomasr98@gmail.com>
2024-10-20 17:36:58 -0300

    1.2.0 - Adding events filters

--- src/cli.py
+++ src/cli.py
@@ -6,12 +6,34 @@ from http.client import HTTPSConnection
 
 
 @click.command()
-@click.version_option(version="1.1.0", prog_name="Github User Activity")
+@click.version_option(version="1.2.0", prog_name="Github User Activity")
 @click.argument("username", type=str)
-def cli(username):
+@click.option("--event", type=str, help="FIlter events by type (push, watch, create, delete, fork, issue, release, pr)",)
+def cli(username, event):
     """
     Fetches and displays recent activity for the specified GitHub username.
     """
+    list_events = ['push', 'watch', 'create', 'delete', 'fork', 'issue', 'release', 'pr']
+
+    # Validate the type of event provided
+    if event and event not in list_events:
+        raise click.ClickException(f"Invalid event type '{event}'. Valid options are: {', '.join(list_events)}")
+
+    # Event type mapping (what the user passes -> the actual name of the event in GitHub)
+    event_map = {
+        'push': 'PushEvent',
+        'watch': 'WatchEvent',
+        'create': 'CreateEvent',
+        'delete': 'DeleteEvent',
+        'fork': 'ForkEvent',
+        'issue': 'IssueEvent',
+        'release': 'ReleaseEvent',
+        'pr': 'PullRequestEvent'
+    }
+
+    # Event mapping if one was provided
+    event_type = event_map.get(event)
+
     try:
         connection = HTTPSConnection("api.github.com", timeout=10)
 
@@ -35,6 +57,9 @@ def cli(username):
             click.echo("No recent activity found.")
             return
 
+        # Filtered events counter
+        matched_events = 0
+
         # Events dictionary
         event_handlers = {
             "PushEvent": lambda e: push_event_message(e['payload']['size'], e['repo']['name']),
@@ -47,11 +72,19 @@ def cli(username):
             "PullRequestEvent": lambda e: pull_request_event_message(e["payload"]["action"], e["repo"]["name"])
         }
 
-        # Show events
-        for event in events:
-            handler = event_handlers.get(event["type"])
+        # Show events and count events matching the filter
+        for github_event in events:
+            if event_type and github_event["type"] != event_type:
+                continue
+
+            handler = event_handlers.get(github_event["type"])
             if handler:
-                click.echo(handler(event))
+                click.echo(handler(github_event))
+                matched_events += 1
+
+        # Check if any events were found with the filter
+        if matched_events == 0:
+            click.echo(f"No activity found for event type: {event}")
 
     except socket.timeout:
         raise click.ClickException("Request timed out. Please try again later")
